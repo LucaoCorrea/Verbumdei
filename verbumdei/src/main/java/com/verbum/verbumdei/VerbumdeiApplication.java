069579ccf59327b3d571f6fc87f6f0f68fc0e146
LucaoCorrea <lucascorrea1505@gmail.com>
2025-08-27 11:14:13 -0300

    project structure

---
+++ verbumdei/src/main/java/com/verbum/verbumdei/VerbumdeiApplication.java
@@ -0,0 +1,13 @@
+package com.verbum.verbumdei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbumdeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbumdeiApplication.class, args);
+	}
+
+}
---
+++ verbumdei/src/test/java/com/verbum/verbumdei/VerbumdeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verbum.verbumdei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbumdeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
